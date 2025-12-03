d958ea6bc66cdb0fef130e4612a3bd8a8d20f5ad
Prakhar Ujjaliya <prakharujjaliya03@gmail.com>
2025-12-03 17:23:07 +0530

    backend

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+const app = express();
+
+// app.use(cors());
+const port = 5000;
+
+const data = {
+    student: [
+        { name: "a", rollno: "20894" },
+        { name: "b", rollno: "20895" }
+    ]
+};
+
+app.get('/', (req, res) => {
+    res.json(data);
+});
+
+
+app.listen(port, () => {
+    console.log(`Backend is running on port http://localhost:${port}`);
+});
