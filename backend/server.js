e4e10e7f23d926e91a32e613b60b69e1c452492b
Prakhar Ujjaliya <prakharujjaliya03@gmail.com>
2025-12-03 18:12:48 +0530

    545

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 const app = express();
 
-// app.use(cors());
+app.use(cors());
 const port = 5000;
 
 const data = {
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-<<<<<<<<< Temporary merge branch 1
-import { useState } from 'react'
+import React, { useEffect, useState } from "react";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import NavBar from '/components/NavBar.jsx'
